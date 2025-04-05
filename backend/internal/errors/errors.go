3aafef2f1c51c42ce9b2342fd3eed02f1501cf7e
enkutatash <enkutatasheshetu96@gmail.com>
2025-04-05 12:00:10 +0300

    structure and db integration

---
+++ backend/internal/errors/errors.go
@@ -0,0 +1,5 @@
+package errors
+
+type AuthorizationError struct {
+	Message string `json:"message"`
+}
---
+++ backend/internal/infrastructure/postgresdb.go
@@ -0,0 +1,39 @@
+package infrastructure
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func NewDB() (*sql.DB, error) {
+	err := godotenv.Load("D:/a2sv_hub/AASTU-G5-T2/backend/.env")
+	if err != nil {
+		log.Println("No .env file found, using system env vars")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	return db, nil
+}
