068360211060dd13a807811eca23844f77dc1049
Rediet Yemane <redietamare04@gmail.com>
2025-04-24 11:51:52 +0300

    commit

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,13 +1,24 @@
 package config
 
+import (
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
+
 type Config struct {
     APIKey    string
     APISecret string
 }
 
 func LoadConfig() Config {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
     return Config{
-        APIKey:    "b9aeda80a867a8216dfcc5e13dd5da7632d7b6e7",    // Replace with actual API key
-        APISecret: "dec6564b62ecc5bab6cd0f9ae21e550c99ae3549", // Replace with actual API secret
+        APIKey:    os.Getenv("CODEFORCES_API_KEY"),
+        APISecret: os.Getenv("CODEFORCES_API_SECRET"),
     }
 }
