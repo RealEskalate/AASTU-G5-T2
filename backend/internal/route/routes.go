3e52cb00ef2b7cbbb7dad6afdc1bfcc34f213367
enkutatash <enkutatasheshetu96@gmail.com>
2025-04-25 01:13:17 +0300

    cors error

--- backend/internal/route/routes.go
+++ backend/internal/route/routes.go
@@ -22,7 +22,7 @@ func SetupRouter(db *sql.DB) *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"Content-Length"},
