4ef085199cfd7390a30b1fa5fbe6577d51ceec63
enkutatash <enkutatasheshetu96@gmail.com>
2025-04-25 16:00:49 +0300

    problems route update

--- backend/internal/route/problem_route.go
+++ backend/internal/route/problem_route.go
@@ -12,13 +12,12 @@ import (
 
 func setupProblemRoutes(router *gin.Engine, controller controller.ProblemController, tokenService utils.TokenService) {
 	problemRoutes := router.Group("/problems")
-	// problemRoutes.Use(middleware.RoleMiddleWare(tokenService, "head", "super_admin", "student"))
+	problemRoutes.Use(middleware.AuthMiddleWare(tokenService), middleware.RoleMiddleWare(tokenService, "head", "super_admin", "student"))
 	problemRoutes.GET("/", controller.GetAllProblems)
 	problemRoutes.GET("/:problem_id", controller.GetAllProblems)
 
 	problemRoutesHead := router.Group("/problems")
-	problemRoutes.Use(middleware.AuthMiddleWare(tokenService), middleware.RoleMiddleWare(tokenService, "head", "super_admin", "student"))
-
+	problemRoutesHead.Use(middleware.AuthMiddleWare(tokenService), middleware.RoleMiddleWare(tokenService, "head", "super_admin"))
 	problemRoutesHead.POST("/", controller.AddProblem)
 	problemRoutesHead.PUT("/addTrack", controller.AddProblemToTrack)
 
